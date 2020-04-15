aaca44c892eb3b9c1e6e1840f0e3c9415847475d
Silas Pockendahl Christensen <pockendahl@hotmail.com>
2020-04-15 15:29:00 +0200

    CopyDensity and SteamVR poses

---
+++ Components/Demo/Grenade.cs
@@ -0,0 +1,96 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+using MeshUtils;
+using static MeshUtils.API;
+using static MeshUtils.VectorUtil;
+
+namespace MeshUtils {
+
+    public class Grenade : MonoBehaviour {
+
+        
+        public SteamVR_Action_Boolean onDetachAction;
+        
+        bool triggered = false;
+        int ttl = 180;
+
+        void Start() {
+            onDetachAction.AddOnStateUpListener(OnDetach, SteamVR_Input_Sources.Any);
+        }
+
+        void OnDetach(SteamVR_Action_Boolean action, SteamVR_Input_Sources src) {
+            triggered = true;
+        }
+
+        void Update() {
+            if (triggered) ttl--;
+            if (ttl == 0) StartCoroutine(Explode());
+        }
+
+        IEnumerator Explode() {
+            List<Rigidbody> rbs = new List<Rigidbody>();
+            foreach (Collider col in Physics.OverlapSphere(transform.position,3)) {
+                if (col.gameObject.tag != "Shootable") continue;
+                foreach (GameObject obj in IterativeCut(col.gameObject,2)) {
+                    Rigidbody rb;
+                    if (obj.TryGetComponent<Rigidbody>(out rb))
+                        rbs.Add(rb);
+                }
+            }
+            yield return null;
+            foreach (Rigidbody rb in rbs)
+                rb.AddExplosionForce(40, transform.position, 3, 0.5f);
+            Destroy(gameObject);
+        }
+
+        List<GameObject> IterativeCut(GameObject obj, int count) {
+            CutParams param = new CutParams(false,false,true,false,false,false,Vector3.zero,float.PositiveInfinity,0,Vector3.zero);
+            try {
+                CutResult res = API.PerformCut(obj,CuttingPlane.RandomInWorldSpace(obj.transform.position),param);
+                if (res == null) return new List<GameObject>();
+                if (count > 1) {
+                    List<GameObject> ret = new List<GameObject>();
+                    foreach (CutObj robj in res.results)
+                        ret.AddRange(
+                            IterativeCut(
+                                robj
+                                    .CopyParent()
+                                    .CopyMaterial()
+                                    .CopyVelocity()
+                                    .CopyDensity()
+                                    .WithCollider()
+                                    .WithDriftVelocity(0.2f)
+                                    .Instantiate(),
+                                count - 1
+                            )
+                        );
+                    return ret;
+                } else return res.results.ConvertAll(
+                    robj =>
+                        robj
+                            .CopyParent()
+                            .CopyMaterial()
+                            .WithCollider()
+                            .WithRenderer()
+                            .CopyVelocity()
+                            .CopyDensity()
+                            .WithDriftVelocity(0.2f)
+                            .Instantiate()
+                );
+            } catch (MeshUtilsException e) {
+                Debug.LogWarning(e);
+                obj.AddComponent<Rigidbody>();
+                obj.AddComponent<MeshRenderer>();
+                obj.AddComponent<MeshCollider>();
+                return new List<GameObject>() {obj};
+            }
+        }
+
+    }
+
+}
---
+++ Components/Demo/Pistol.cs
@@ -0,0 +1,82 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+using MeshUtils;
+using static MeshUtils.API;
+using static MeshUtils.VectorUtil;
+
+namespace MeshUtils {
+
+    public class Pistol : MonoBehaviour {
+
+        public SteamVR_Action_Boolean pullTriggerAction;
+
+        void Start() {
+            pullTriggerAction.AddOnStateDownListener(TriggerPulled, SteamVR_Input_Sources.Any);
+        }
+
+        void TriggerPulled(SteamVR_Action_Boolean action, SteamVR_Input_Sources src) {
+            RaycastHit rayHit;
+            Ray ray = new Ray(transform.position + transform.up * 0.025f, -transform.forward);
+            if (
+                Physics.Raycast(
+                    ray,
+                    out rayHit,
+                    100
+                )
+            ) {
+                // Debug.DrawRay(ray.origin, ray.direction * rayHit.distance, Color.white, 0.25f);
+                GameObject obj = rayHit.collider.gameObject;
+                if (obj.tag == "Shootable") {
+                    IterativeCut(obj,3).ForEach(r=>r.GetComponent<Rigidbody>().velocity += 5 * ray.direction);
+                }
+            }
+        }
+
+        List<GameObject> IterativeCut(GameObject obj, int count) {
+            CutParams param = new CutParams(false,false,true,false,false,false,Vector3.zero,float.PositiveInfinity,0,Vector3.zero);
+            try {
+                CutResult res = API.PerformCut(obj,CuttingPlane.RandomInWorldSpace(obj.transform.position),param);
+                if (res == null) return new List<GameObject>();
+                if (count > 1) {
+                    List<GameObject> ret = new List<GameObject>();
+                    foreach (CutObj robj in res.results)
+                        ret.AddRange(
+                            IterativeCut(
+                                robj
+                                    .CopyParent()
+                                    .CopyMaterial()
+                                    .CopyVelocity()
+                                    .WithDriftVelocity(0.2f)
+                                    .Instantiate(),
+                                count - 1
+                            )
+                        );
+                    return ret;
+                } else return res.results.ConvertAll(
+                    robj =>
+                        robj
+                            .CopyParent()
+                            .CopyMaterial()
+                            .WithCollider()
+                            .WithRenderer()
+                            .CopyVelocity()
+                            .WithDriftVelocity(0.2f)
+                            .Instantiate()
+                );
+            } catch (MeshUtilsException e) {
+                Debug.LogWarning(e);
+                obj.AddComponent<Rigidbody>();
+                obj.AddComponent<MeshRenderer>();
+                obj.AddComponent<MeshCollider>();
+                return new List<GameObject>() {obj};
+            }
+        }
+
+    }
+
+}
