bf594a1d697b2a39cdaaf471a3fc9c1efd663ff7
Silas Pockendahl Christensen <pockendahl@hotmail.com>
2020-04-16 20:17:24 +0200

    Merge fix

--- Examples/Common/ComplexKnife.cs
+++ Examples/Common/ComplexKnife.cs
@@ -57,7 +57,7 @@ namespace MeshUtils {
 
         void OnTriggerExit(Collider col) {
             template.PrepareTmp();
-            template.Draw();
+            template.DrawDebug();
             var res = API.tmp(col.gameObject,template);
             if (res != null) {
                 foreach (var r in res.results) {
